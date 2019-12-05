e97310aa528e77e25a82153d91853ccacea2863c
Lyle Dickie <lyle.e.dickie@gmail.com>
2019-12-04 20:44:54 -0500

    Clean up duplicate files in backend filetree

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-<<<<<<< HEAD
-  attributes :id
-=======
   attributes :id, :email, :password_digest
->>>>>>> 447c3456f8953064de40ed02caee56562c7404e6
 end
